9e7ecb35c7dd2c2851693a1d347f200cd032a8e6
pARHA-CS <raphael.mercier-2@etu.univ-tours.fr>
2024-12-06 15:33:44 +0100

    ajout de fichier -> en pleine reflexion

---
+++ scraping/scraping_donnees_joueurs.py
@@ -0,0 +1,8 @@
+from requests import get
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+import json
+from typing import TypedDict
+import os
+import time
+from typing import Tuple
